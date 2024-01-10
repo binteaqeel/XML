a7d7980e51492aa1cb7ca0a86dc86cdd6daf9b25
Fatima Binte Aqeel <binteaqeel2004@gmail.com>
2024-01-10 18:17:55 +0500

    savemyallwork

--- README.md
+++
@@ -1,2 +0,0 @@
-![task1](https://github.com/binteaqeel/XML/assets/154437463/84ed0a0d-24d6-48e6-b2dc-269d971e4dd5)
-![task2](https://github.com/binteaqeel/XML/assets/154437463/95078a0f-4893-4dac-bb86-50b0225781f7)
---
+++ one.js
@@ -0,0 +1,17 @@
+var organization =[
+    {
+        id :0,
+        name : "Fatima",
+        designation : "Manager",
+        salary : 1000000,
+        phone_no : 342756278
+    },
+
+    {
+        id :1,
+        name : "Maryam",
+        designation : "General Manager",
+        salary : 2000000,
+        phone_no : 375756278
+    },
+]
